91c33a54fc1a6577947654291090df2f1db54c86
nizkopoklonnyi <tolia150993@mail.ru>
2016-08-02 07:09:01 +0700

    fizzbuzz check

--- src/fizzbuzz/com/FizzBuzz.java
+++ src/fizzbuzz/com/FizzBuzz.java
@@ -7,18 +7,40 @@ import output.com.Presentation;
 public class FizzBuzz {
 	
 	private Presentation presentation;
-	private String fizz="Fizz", buzz="Buzz";
-
+	private String fizz="Fizz", buzz="Buzz",fizzbuzz="fizzbuzz";
+	private boolean fizzbuzz_flag=false, numb=true;
+	
 	public FizzBuzz(){
 		presentation= new Presentation();
 	} 
 	
 	public void check(int number){
-		if(number%3==0)
-			presentation.print(fizz);
-		if(number %5==0)
-            presentation.print(buzz );
-		if(number %3!=0 && number%5!=0)
+		
+		fizzbuzz_flag=false;
+		numb= true;
+		
+		//check number div by 3 and 5 simultaneously
+		if(number %3==0 && number%5==0){
+			presentation.print(fizzbuzz);
+			fizzbuzz_flag=true;
+			numb=false;
+		}
+		
+		// if number don't div by 3 and 5 simultaneously
+		if(fizzbuzz_flag==false){
+			
+			if(number%3==0){
+				presentation.print(fizz);
+				numb= false;
+			}
+			if(number %5==0){
+				presentation.print(buzz );
+				numb= false;
+			}
+		}
+		
+		//if !fizz and !buzz and !fizzbuzz
+		if(numb== true)
 			presentation.print(((Integer)number).toString());
 		
 			
